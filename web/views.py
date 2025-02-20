493e3c3962e3fbed6318614860a5233dd6b4b670
solanomillo <solanomillo144@gmail.con>
2025-02-20 17:33:39 -0300

    modulo3

--- web/views.py
+++ web/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Categoria, Producto
+from .carrito import Cart
 
 """ VISTAS PARA EL CATALOGO DEL PRODUCTO """
 
@@ -46,3 +47,40 @@ def detalleProducto(request, producto_id):
     return render(request, 'producto.html', {
         'producto': objProducto
     })
+
+
+""" VISTAS PARA EL CARRITO DE COMPRAS """
+
+
+def carrito(request):
+    return render(request, 'carrito.html', {})
+
+
+def agregarCarrito(request, producto_id):
+    if request.method == 'POST':
+        cantidad = int(request.POST['cantidad'])
+    else:
+        cantidad = 1
+    objProducto = Producto.objects.get(pk=producto_id)
+    carritoProducto = Cart(request)
+    carritoProducto.add(objProducto, cantidad)
+
+    if request.method == 'GET':
+        return redirect('/')
+
+    return render(request, 'carrito.html', {})
+
+
+def eliminarProductoCarrito(request, producto_id):
+    objProducto = Producto.objects.get(pk=producto_id)
+    carritoProducto = Cart(request)
+    carritoProducto.delete(objProducto)
+
+    return render(request, 'carrito.html', {})
+
+
+def limpiarCarrito(request):
+    carritoProducto = Cart(request)
+    carritoProducto.clear()
+
+    return render(request, 'carrito.html', {})
